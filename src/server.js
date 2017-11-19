4c003a8f5ed1b3f1328c52a731057aca7046eec5
Jeff Hoffman <jhoffman@trilogyed.com>
2017-11-19 05:18:07 +0000

    Added CORS proxy

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 var express = require("express");
+var corsAnywhere = require("cors-anywhere");
 
-var port = process.env.PORT || 8080;
+var host = process.env.IP || '0.0.0.0';
+var port = parseInt(process.env.PORT) || 8080;
+var proxyPort = port + 1
 
 var app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.listen(port, function() {
-    console.log("==> Static server listening on port " + port);
+app.listen(port, host, function() {
+    console.log('==> Static server listening on ' + host + ':' + port);
+});
+
+corsAnywhere.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(proxyPort, host, function() {
+    console.log('==> CORS Anywhere proxy listening on ' + host + ':' + proxyPort);
 });
