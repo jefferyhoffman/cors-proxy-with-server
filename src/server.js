d4e34b3d97043128abd7d703ea1a8432611d6dc0
Jeff Hoffman <jhoffman@trilogyed.com>
2017-11-19 04:53:55 +0000

    Added Express static server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+app.use(express.static("public"));
+
+app.listen(port, function() {
+    console.log("==> Static server listening on port " + port);
+});
